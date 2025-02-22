2642b4c6bf2dc4c87022bc7c293663c426211f1b
Aleida Diaz-Roque <aleidaroque.adr@gmail.com>
2025-02-22 15:34:31 -0800

    Update BotEventChecker.h

--- Final Project/src/BotEventChecker.h
+++ Final Project/src/BotEventChecker.h
@@ -1,8 +1,9 @@
 /* 
  * File:   BotEventChecker.h
  * Author: Gabriel Hugh Elkaim
+ * Modified by: Aleida Diaz-Roque
  *
- * Template file to set up typical EventCheckers for the  Events and Services
+ * File to set up typical EventCheckers for the  Events and Services
  * Framework (ES_Framework) on the Uno32 for the CMPE-118/L class. Note that
  * this file will need to be modified to fit your exact needs, and most of the
  * names will have to be changed to match your code.
@@ -43,20 +44,6 @@
  * PUBLIC FUNCTION PROTOTYPES                                                  *
  ******************************************************************************/
 
-/**
- * @Function CheckBattery(void)
- * @param none
- * @return TRUE or FALSE
- * @brief This function is a prototype event checker that checks the battery voltage
- *        against a fixed threshold (#defined in the .c file). Note that you need to
- *        keep track of previous history, and that the actual battery voltage is checked
- *        only once at the beginning of the function. The function will post an event
- *        of either BATTERY_CONNECTED or BATTERY_DISCONNECTED if the power switch is turned
- *        on or off with the USB cord plugged into the Uno32. Returns TRUE if there was an 
- *        event, FALSE otherwise.
- * @note Use this code as a template for your other event checkers, and modify as necessary.
- * @author Gabriel H Elkaim, 2013.09.27 09:18
- * @modified Gabriel H Elkaim/Max Dunne, 2016.09.12 20:08 */
 uint8_t CheckBattery(void);
 
 uint8_t CheckTape(void);
