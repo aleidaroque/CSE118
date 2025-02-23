4d93747d1549f0f3ed9f2af2a9483a6ea05c47be
Aleida Diaz-Roque <aleidaroque.adr@gmail.com>
2025-02-22 18:31:45 -0800

    Update SearchForBeaconSubHSM.c

--- Final Project/src/SearchForBeaconSubHSM.c
+++ Final Project/src/SearchForBeaconSubHSM.c
@@ -1,20 +1,18 @@
 /*
- * File: TemplateSubHSM.c
+ * File: SearchForBeaconSubHSM.c
  * Author: J. Edward Carryer
  * Modified: Gabriel H Elkaim
+ * Author of Current Version: Aleida Diaz-Roque
  *
- * Template file to set up a Heirarchical State Machine to work with the Events and
+ * File to set up a Heirarchical State Machine to work with the Events and
  * Services Framework (ES_Framework) on the Uno32 for the CMPE-118/L class. Note that
  * this file will need to be modified to fit your exact needs, and most of the names
  * will have to be changed to match your code.
  *
- * There is for a substate machine. Make sure it has a unique name
- *
- * This is provided as an example and a good place to start.
- *
  * History
  * When           Who     What/Why
  * -------------- ---     --------
+ * 05/15/24 10:00 adr      Modified template for ECE 118 Spring 2024 project
  * 09/13/13 15:17 ghe      added tattletail functionality and recursive calls
  * 01/15/12 11:12 jec      revisions for Gen2 framework
  * 11/07/11 11:26 jec      made the queue static
@@ -100,7 +98,7 @@ static int collisionFrom = START;
  ******************************************************************************/
 
 /**
- * @Function InitTemplateSubHSM(uint8_t Priority)
+ * @Function InitSearchForBeaconSubHSM(uint8_t Priority)
  * @param Priority - internal variable to track which event queue to use
  * @return TRUE or FALSE
  * @brief This will get called by the framework at the beginning of the code
@@ -121,7 +119,7 @@ uint8_t InitSearchForBeaconSubHSM(void) {
 }
 
 /**
- * @Function RunTemplateSubHSM(ES_Event ThisEvent)
+ * @Function RunSearchForBeaconSubHSM(ES_Event ThisEvent)
  * @param ThisEvent - the event (type and param) to be responded.
  * @return Event - return event (type and param), in general should be ES_NO_EVENT
  * @brief This function is where you implement the whole of the heirarchical state
@@ -129,12 +127,12 @@ uint8_t InitSearchForBeaconSubHSM(void) {
  *        queue. This function will be called recursively to implement the correct
  *        order for a state transition to be: exit current state -> enter next state
  *        using the ES_EXIT and ES_ENTRY events.
- * @note Remember to rename to something appropriate.
- *       The lower level state machines are run first, to see if the event is dealt
+ * @note The lower level state machines are run first, to see if the event is dealt
  *       with there rather than at the current level. ES_EXIT and ES_ENTRY events are
  *       not consumed as these need to pass pack to the higher level state machine.
  * @author J. Edward Carryer, 2011.10.23 19:25
- * @author Gabriel H Elkaim, 2011.10.23 19:25 */
+ * @author Gabriel H Elkaim, 2011.10.23 19:25 
+ * @author Aleida Diaz-Roque */
 ES_Event RunSearchForBeaconSubHSM(ES_Event ThisEvent) {
     uint8_t makeTransition = FALSE; // use to flag transition
     SearchForBeaconSubHSMState_t nextState; // <- change type to correct enum
@@ -156,6 +154,8 @@ ES_Event RunSearchForBeaconSubHSM(ES_Event ThisEvent) {
                 ThisEvent.EventType = ES_NO_EVENT;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
         case RotateSearch: // in the first state, replace this with correct names
 
@@ -192,6 +192,8 @@ ES_Event RunSearchForBeaconSubHSM(ES_Event ThisEvent) {
                     break;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
         case InfinitySearchRight: // in the first state, replace this with correct names
 
@@ -286,6 +288,8 @@ ES_Event RunSearchForBeaconSubHSM(ES_Event ThisEvent) {
                     break;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
         case InfinitySearchLeft: // in the first state, replace this with correct names
 
@@ -386,6 +390,8 @@ ES_Event RunSearchForBeaconSubHSM(ES_Event ThisEvent) {
                     break;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
         case DriveToBeacon: // in the first state, replace this with correct names
             moveSlug(DRIVE_SPEED);
@@ -438,6 +444,8 @@ ES_Event RunSearchForBeaconSubHSM(ES_Event ThisEvent) {
                     break;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
         case Park:
             switch (ThisEvent.EventType) {
@@ -473,6 +481,8 @@ ES_Event RunSearchForBeaconSubHSM(ES_Event ThisEvent) {
                     break;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
         case Reverse:
 
@@ -540,6 +550,8 @@ ES_Event RunSearchForBeaconSubHSM(ES_Event ThisEvent) {
 
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
         case Turning: // in the first state, replace this with correct names
 
@@ -640,6 +652,8 @@ ES_Event RunSearchForBeaconSubHSM(ES_Event ThisEvent) {
                     break;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
         case ShortDrive: // in the first state, replace this with correct names
             switch (ThisEvent.EventType) {
