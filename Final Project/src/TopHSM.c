95dc505457c673d65efac95d7da001d03ea22f8a
Aleida Diaz-Roque <aleidaroque.adr@gmail.com>
2025-02-22 18:53:57 -0800

    Update TopHSM.c

--- Final Project/src/TopHSM.c
+++ Final Project/src/TopHSM.c
@@ -1,21 +1,19 @@
 /*
- * File: TemplateSubHSM.c
+ * File: TopHSM.c
  * Author: J. Edward Carryer
  * Modified: Gabriel Elkaim and Soja-Marie Morgens
+ * Author of Current Version: Aleida Diaz-Roque
  *
- * Template file to set up a Heirarchical State Machine to work with the Events and
+ * File to set up a Heirarchical State Machine to work with the Events and
  * Services Framework (ES_Framework) on the Uno32 for the CMPE-118/L class. Note that
  * this file will need to be modified to fit your exact needs, and most of the names
  * will have to be changed to match your code.
  *
- * There is another template file for the SubHSM's that is slightly differet, and
- * should be used for all of the subordinate state machines (flat or heirarchical)
- *
- * This is provided as an example and a good place to start.
  *
  * History
  * When           Who     What/Why
  * -------------- ---     --------
+ * 05/15/24 10:00 adr	   modified template for ECE118 Spring 2024 project
  * 09/13/13 15:17 ghe      added tattletail functionality and recursive calls
  * 01/15/12 11:12 jec      revisions for Gen2 framework
  * 11/07/11 11:26 jec      made the queue static
@@ -86,7 +84,7 @@ static uint8_t MyPriority;
  ******************************************************************************/
 
 /**
- * @Function InitTemplateHSM(uint8_t Priority)
+ * @Function InitTopHSM(uint8_t Priority)
  * @param Priority - internal variable to track which event queue to use
  * @return TRUE or FALSE
  * @brief This will get called by the framework at the beginning of the code
@@ -94,7 +92,7 @@ static uint8_t MyPriority;
  *        queue, which will be handled inside RunTemplateFSM function. Remember
  *        to rename this to something appropriate.
  *        Returns TRUE if successful, FALSE otherwise
- * @author J. Edward Carryer, 2011.10.23 19:25 */
+ * @author Aleida Diaz-Roque */
 uint8_t InitTopHSM(uint8_t Priority) {
     MyPriority = Priority;
     // put us into the Initial PseudoState
@@ -108,7 +106,7 @@ uint8_t InitTopHSM(uint8_t Priority) {
 }
 
 /**
- * @Function PostTemplateHSM(ES_Event ThisEvent)
+ * @Function PostTopHSM(ES_Event ThisEvent)
  * @param ThisEvent - the event (type and param) to be posted to queue
  * @return TRUE or FALSE
  * @brief This function is a wrapper to the queue posting function, and its name
@@ -121,7 +119,7 @@ uint8_t PostTopHSM(ES_Event ThisEvent) {
 }
 
 /**
- * @Function RunTemplateHSM(ES_Event ThisEvent)
+ * @Function RunTopHSM(ES_Event ThisEvent)
  * @param ThisEvent - the event (type and param) to be responded.
  * @return Event - return event (type and param), in general should be ES_NO_EVENT
  * @brief This function is where you implement the whole of the heirarchical state
@@ -134,7 +132,8 @@ uint8_t PostTopHSM(ES_Event ThisEvent) {
  *       with there rather than at the current level. ES_EXIT and ES_ENTRY events are
  *       not consumed as these need to pass pack to the higher level state machine.
  * @author J. Edward Carryer, 2011.10.23 19:25
- * @author Gabriel H Elkaim, 2011.10.23 19:25 */
+ * @author Gabriel H Elkaim, 2011.10.23 19:25 
+ * @author Aleida Diaz-Roque */
 ES_Event RunTopHSM(ES_Event ThisEvent) {
     uint8_t makeTransition = FALSE; // use to flag transition
     TopHSMState_t nextState; // <- change type to correct enum
@@ -168,11 +167,10 @@ ES_Event RunTopHSM(ES_Event ThisEvent) {
                 ;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-        case SearchForBeacon: // in the first state, replace this with correct names
-            // run sub-state machine for this state
-            //NOTE: the SubState Machine runs and responds to events before anything in the this
-            //state machine does
+        case SearchForBeacon: 
             ThisEvent = RunSearchForBeaconSubHSM(ThisEvent);
             switch (ThisEvent.EventType) {
                 case ES_ENTRY:
@@ -200,11 +198,11 @@ ES_Event RunTopHSM(ES_Event ThisEvent) {
                     break;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-        case Collection1: // in the first state, replace this with correct names
-            // run sub-state machine for this state
-            //NOTE: the SubState Machine runs and responds to events before anything in the this
-            //state machine does
+        case Collection1: 
+		
             ThisEvent = RunCollection1SubHSM(ThisEvent);
 
             switch (ThisEvent.EventType) {
@@ -226,11 +224,10 @@ ES_Event RunTopHSM(ES_Event ThisEvent) {
                     break;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-        case Collection2: // in the first state, replace this with correct names
-            // run sub-state machine for this state
-            //NOTE: the SubState Machine runs and responds to events before anything in the this
-            //state machine does
+        case Collection2:
 
             ThisEvent = RunCollection2SubHSM(ThisEvent);
 
@@ -240,13 +237,6 @@ ES_Event RunTopHSM(ES_Event ThisEvent) {
 
                 case ES_EXIT:
                     break;
-
-//                case AT_BEACON_TOWER:
-//                    printf("\r\n Beacon Transition");
-//                    nextState = SearchForBeacon;
-//                    makeTransition = TRUE;
-//                    ThisEvent.EventType = ES_NO_EVENT;
-//                    break;
 
                 case READY_TO_DEPOSIT:
                   
@@ -264,12 +254,11 @@ ES_Event RunTopHSM(ES_Event ThisEvent) {
                     break;
             }
             break;
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+	    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
             
-        case Deposit: // in the first state, replace this with correct names
-            // run sub-state machine for this state
-            //NOTE: the SubState Machine runs and responds to events before anything in the this
-            //state machine does
+        case Deposit: 
 
             ThisEvent = RunDepositSubHSM(ThisEvent);
 
