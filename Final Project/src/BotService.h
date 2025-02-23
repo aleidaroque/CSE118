a96d1b31c644c5e005dc7529d34fc91aaf181c34
Aleida Diaz-Roque <aleidaroque.adr@gmail.com>
2025-02-22 16:03:43 -0800

    Update BotService.h

--- Final Project/src/BotService.h
+++ Final Project/src/BotService.h
@@ -3,7 +3,7 @@
  * Author: J. Edward Carryer
  * Modified: Gabriel H Elkaim, Aleida Diaz-Roque
  *
- * Template file to set up a simple service to work with the Events and Services
+ * File to set up a simple service to work with the Events and Services
  * Framework (ES_Framework) on the Uno32 for the CMPE-118/L class. Note that this file
  * will need to be modified to fit your exact needs, and most of the names will have
  * to be changed to match your code.
